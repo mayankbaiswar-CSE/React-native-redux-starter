8538adcd366940d80450f389e7877dac59dcb82f
Mayank Baiswar <mayank.baiswar@daffodilsw.com>
2017-05-31 13:13:49 +0530

    initiall commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'React';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import Header from './components/common/header';
+
+const App = () => {
+    return (
+        <Provider store={createStore(reducers)}>
+            <View>
+                <Header headerText={"Tech Stack"} />
+            </View>
+        </Provider>
+    );
+}
+
+export default App;
